eb5298b9f0b85a6497dc0a6f030d81543149f6ee
Polina Sidorina <polishas206@gmail.com>
2026-01-30 18:43:41 +0300

    githubPages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import QuestsPage from './pages/QuestsPage/QuestsPage';
 const App = function () {
   return (
     <Routes>
+      <Route path="/#" />
       <Route path={'/home'} element={<HomePage />}></Route>
       <Route path={'/budget'} element={<QuestsPage />}></Route>
       <Route path={'/quests'} element={<QuestsPage />}></Route>
